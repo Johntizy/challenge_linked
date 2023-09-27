198b48e40db97c304243d17802b4289c6330330f
unknown <jtaiwo909@gmail.com>
2023-09-27 05:40:52 +0100

    Code Destructure

--- src/containers/Hero/Hero.jsx
+++ src/containers/Hero/Hero.jsx
@@ -8,9 +8,11 @@ const Hero = () => {
   return (
     <div className="app_hero">
       <div className="app_hero-top">
-        <p className="p_montserrat">
-          Igniting a Revolution in <span>HR Innovation</span>
-        </p>
+        <Slide direction="right">
+          <p className="p_montserrat">
+            Igniting a Revolution in <span>HR Innovation</span>
+          </p>
+        </Slide>
       </div>
 
       <div className="app__hero-wrapper flex_center">
@@ -24,19 +26,21 @@ const Hero = () => {
             </h1>
           </Slide>
           <Slide direction="up" delay={300}>
-          <h1 className="headtext" style={{ display: "flex" }}>
-            Hackathon <span style={{ marginLeft: "1rem" }}>1.0</span>
-            <img src={images.chain} />
-            <img src={images.fire} />
-          </h1>
+            <h1 className="headtext" style={{ display: "flex" }}>
+              Hackathon <span style={{ marginLeft: "1rem" }}>1.0</span>
+              <img src={images.chain} />
+              <img src={images.fire} />
+            </h1>
           </Slide>
           <Slide direction="up" delay={700}>
-          <p className="p_montserrat">
-            Participate in getlinked tech Hackathon 2023 stand a chance to win a
-            Big prize
-          </p></Slide>
+            <p className="p_montserrat">
+              Participate in getlinked tech Hackathon 2023 stand a chance to win
+              a Big prize
+            </p>
+          </Slide>
           <Slide direction="up" delay={1200}>
-          <button className="custom_button">Register</button></Slide>
+            <button className="custom_button">Register</button>
+          </Slide>
 
           <h1 className="timer">
             00<span>H</span> 00<span>M</span> 00<span>S</span>{" "}
@@ -61,10 +65,18 @@ const Hero = () => {
         <img src={images.lens} alt="gradient-top" />
       </div>
 
-      <img src={images.star} alt="gradient-top" className="absolute_star stars" />
-      <img src={images.prestar} alt="gradient-top" className="absolute_star2 stars" />
+      <img
+        src={images.star}
+        alt="gradient-top"
+        className="absolute_star stars"
+      />
+      <img
+        src={images.prestar}
+        alt="gradient-top"
+        className="absolute_star2 stars"
+      />
     </div>
   );
-}
+};
 
 export default Hero;
